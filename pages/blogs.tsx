e7f26a3819936dc687d9ac889fc0ab5a2e5d8466
putrabungsu <muhammadnorseputra@gmail.com>
2022-11-15 11:22:14 +0700

    add favicons

---
+++ pages/blogs.tsx
@@ -0,0 +1,17 @@
+import Layouts from "@/components/layouts";
+import type { NextPage } from "next";
+import Image from "next/image";
+import Maintenance from '../public/maintenance.png'
+
+const Blogs: NextPage = () => {
+  return (
+  <Layouts>
+    <div className="flex flex-1 flex-col items-center justify-center h-screen">
+        <Image src={Maintenance} width={400} height={240} alt="maintenance featured"/>
+        <h3 className="text-black dark:text-white font-bold text-xl">"Blog" UNDER MAINTENANCE</h3>
+    </div>
+  </Layouts>
+  );
+};
+
+export default Blogs;
