3d55f6a48263c4d95bec77c2caf87f155c887aaf
Muhammad Nor Seputra <43113176+muhammadnorseputra@users.noreply.github.com>
2023-08-10 16:12:48 +0700

    Update blogs.tsx

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import {ArrowsExpandIcon} from "@heroicons/react/outline";
 import { useState } from "react";
-import Labels from "@/components/blogs/labels";
+import Labels from "@/components/blogs/Labels";
 import FeaturedPost from "@/components/blogs/FeaturedPost";
 import PostList from "@/components/blogs/PostList";
 
